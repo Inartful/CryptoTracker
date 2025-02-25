a2f8521d800b95992aeb76f68b5a82e04e95054a
Inartful <ilia.korlyakov@gmail.com>
2025-02-25 19:30:26 +0500

    second commit

---
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,47 @@
+package com.example.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+import com.example.cryptotracker.crypto.domain.Coin
+import com.example.cryptotracker.core.presentation.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCaUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hr: DisplayableNumber,
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCaUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
